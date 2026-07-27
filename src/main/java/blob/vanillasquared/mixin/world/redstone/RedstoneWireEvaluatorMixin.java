c0715d68b10650a66e5adda79277ddde0973368a
PainterFlow11 <163044531+PainterFlow@users.noreply.github.com>
2026-07-27 16:39:51 +0200

    working on redstone changes

--- src/main/java/blob/vanillasquared/mixin/world/redstone/RedstoneWireEvaluatorMixin.java
+++ src/main/java/blob/vanillasquared/mixin/world/redstone/RedstoneWireEvaluatorMixin.java
@@ -7,11 +7,15 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.redstone.RedstoneWireEvaluator;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(RedstoneWireEvaluator.class)
 public abstract class RedstoneWireEvaluatorMixin {
+    @Shadow
+    protected abstract int getWireSignal(BlockPos pos, BlockState state);
+
     @Redirect(
             method = "getIncomingWireSignal",
             at = @At(
@@ -22,6 +26,33 @@ public abstract class RedstoneWireEvaluatorMixin {
     )
     private boolean vsq$transmitsDownward(BlockState state, BlockGetter level, BlockPos pos) {
         return VSQDirectionalRedstoneTransmission.transmitsDownward(state, level, pos);
+    }
+
+    @Redirect(
+            method = "getIncomingWireSignal",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/world/level/redstone/RedstoneWireEvaluator;getWireSignal(Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;)I",
+                    ordinal = 1
+            )
+    )
+    private int vsq$getBidirectionalWireSignal(
+            RedstoneWireEvaluator ignoredEvaluator,
+            BlockPos upperWirePos,
+            BlockState upperWireState,
+            Level level,
+            BlockPos ignoredWirePos
+    ) {
+        int signal = this.getWireSignal(upperWirePos, upperWireState);
+        BlockPos neighborPos = upperWirePos.below();
+        BlockState neighborState = level.getBlockState(neighborPos);
+        if (neighborState.is(VSQDirectionalRedstoneTransmission.TRANSMITS_UPWARD)
+                && neighborState.is(VSQDirectionalRedstoneTransmission.TRANSMITS_DOWNWARD)) {
+            BlockPos lowerWirePos = neighborPos.below();
+            signal = Math.max(signal, this.getWireSignal(lowerWirePos, level.getBlockState(lowerWirePos)));
+        }
+
+        return signal;
     }
 
     @Redirect(
